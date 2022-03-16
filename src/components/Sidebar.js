76326d2b5a30cd8cf3292b68c7201c4762c831dc
Sachin Sahu <sachinksahu.431@gmail.com>
2022-03-16 22:52:23 +0530

    User Manual pdf

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,18 +1,17 @@
-import React from 'react'
-import { slide as Menu } from 'react-burger-menu';
-import './Sidebar.css';
+import React from "react";
+import { slide as Menu } from "react-burger-menu";
+import "./Sidebar.css";
+import pdf from "../static/Lorem_ipsum.pdf";
 
-
-export default props => {
-    return (
-      <Menu>
-        <a className="menu-item" href="/">
-          Home
-        </a>
-        <a className="menu-item" href="/">
-          Tutorial
-        </a>
-        
-      </Menu>
-    );
-  };
+export default (props) => {
+  return (
+    <Menu>
+      <a className="menu-item" href="/">
+        Home
+      </a>
+      <a className="menu-item" href={pdf} target="_blank" rel="noreferrer">
+        User Manual
+      </a>
+    </Menu>
+  );
+};
