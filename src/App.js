c76e54217745449f493e4de58bb0331aaaaacbe5
Sachin Sahu <sachinksahu.431@gmail.com>
2022-03-17 11:01:39 +0530

    UI improved
    Add logo
    Details section spacing
    Footer section

--- src/App.js
+++ src/App.js
@@ -25,9 +25,24 @@ function App() {
           <h4 className="display-6 text-center mb-4">
             <i className="fab fa-react" /> Upload Your File
           </h4>
-          <FileUpload />
         </div>
+        <FileUpload />
+      </div>
+      <div
+        className="container"
+        style={{
+          height: "80vh",
+          width: "80vw",
+        }}
+      >
         <Details></Details>
+      </div>
+      <div className="footer">
+        <div className="row">
+          <div className="col-lg-8 col-md-10 mx-auto">
+            <p className="copyright text-muted">Team ABCD</p>
+          </div>
+        </div>
       </div>
     </div>
   );
